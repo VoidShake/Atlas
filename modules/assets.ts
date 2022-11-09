d6dfdf354035ececb78cec764ced41ceb9c49dc0
PssbleTrngle <nik@somethingcatchy.net>
2022-11-09 22:27:21 +0100

    do not fail on shallow clones

--- modules/assets.ts
+++ modules/assets.ts
@@ -4,6 +4,7 @@ import { defineNuxtModule } from '@nuxt/kit'
 export default defineNuxtModule({
    setup(_, nuxt) {
       nuxt.hook('build:before', () => {
+         if (!existsSync('assets/emblem')) return
          if (!existsSync('public')) mkdirSync('public')
          copyFileSync('assets/emblem/favicon.ico', 'public/favicon.ico')
       })
