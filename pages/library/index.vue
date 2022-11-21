992af94473b0ccc4f428fb6d99a47d7a5b3f51d6
PssbleTrngle <nik@somethingcatchy.net>
2022-11-21 19:11:14 +0100

    add filter to library

--- pages/library/index.vue
+++ pages/library/index.vue
@@ -10,17 +10,27 @@
          <NuxtLink v-for="tale in result.connection.nodes" :key="tale.id" :to="`/library/${tale.id}`">
             <TalePreview :tale="tale" />
          </NuxtLink>
+
+         <template #filter>
+            <PaginatedFilter id="tales" v-model="filter">
+               <FormKit label="Title" name="title" type="text" />
+               <FormKit label="Created After" name="createdAfter" type="date" />
+               <FormKit label="Created Before" name="createdBefore" type="date" />
+            </PaginatedFilter>
+         </template>
       </PaginatedList>
    </section>
 </template>
 
 <script lang="ts" setup>
 import { PencilIcon } from '@heroicons/vue/24/solid'
-import { GetTalesDocument, Permission } from '~/graphql/generated'
+import { GetTalesDocument, Permission, TaleFilter } from '~/graphql/generated'
 
 const { hasPermission } = useSession()
 
+const filter = useState<TaleFilter>(() => ({}))
+
 const limit = ref(24)
 
-const { result, next, previous } = usePagination(GetTalesDocument, limit)
+const { result, next, previous } = usePagination(GetTalesDocument, limit, filter)
 </script>
